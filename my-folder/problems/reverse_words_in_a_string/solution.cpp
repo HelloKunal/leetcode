28985b1b8a69fac55b8961ef2c2ead9bb0f1feca
Kunal <hellokunalpawar@gmail.com>
2021-07-20 12:36:51 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

---
+++ my-folder/problems/reverse_words_in_a_string/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void reverseWord (string &s, int i, int j)
+    {
+        while (i < j)
+        {
+            char t = s[i];
+            s[i++] = s[j];
+            s[j--] = t;
+        }
+    }
+    
+    string reverseWords(string s) {
+        int i = 0, j = 0;
+        int l = 0;
+        int len = s.length();
+        int wordcount = 0;
+        
+        while (true)
+        {
+            while (i < len && s[i] == ' ')
+                i++;
+            
+            if (i == len)
+                break;
+            
+            if (wordcount)
+                s[j++] = ' ';
+            
+            l = j;
+            while (i < len && s[i] != ' ')
+            {
+                s[j] = s[i];
+                j++;
+                i++;
+            }
+            reverseWord (s, l, j - 1);
+            wordcount++;
+        }
+        
+        s.resize (j);
+        reverseWord (s, 0, j - 1);
+        return s;
+    }
+};
