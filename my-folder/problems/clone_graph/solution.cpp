70f0888dc04e53c9400f2b2cdde638024d9864b3
Kunal <hellokunalpawar@gmail.com>
2021-07-22 06:10:22 +0000

    Sync LeetCode submission - Clone Graph (cpp)

--- my-folder/problems/clone_graph/solution.cpp
+++ my-folder/problems/clone_graph/solution.cpp
@@ -1,17 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
         if (!node) {
             return NULL;
         }
-        if (copies.find(node) == copies.end()) {
-            copies[node] = new Node(node -> val, {});
-            for (Node* neighbor : node -> neighbors) {
-                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+        
+        Node* copy = new Node (node->val, {});
+        unordered_map<Node*, Node*> copies;
+        copies[node] = copy;
+        queue<Node*> q;
+        q.push(node);
+        
+        while (!q.empty())
+        {
+            Node* curr = q.front();
+            q.pop();
+            
+            for (Node* neighbor : curr->neighbors)
+            {
+                if (copies.find(neighbor) == copies.end())
+                {
+                    copies[neighbor] = new Node (neighbor->val, {});
+                    q.push(neighbor);                    
+                }
+                copies[curr]->neighbors.push_back(copies[neighbor]);                
             }
         }
-        return copies[node];
+        
+        return copy;
     }
-private:
-    unordered_map<Node*, Node*> copies;
 };
