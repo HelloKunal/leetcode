668ef9162bfba3f118d87ed4ab8209d1b664920f
Kunal <hellokunalpawar@gmail.com>
2021-09-06 12:52:02 +0000

    Sync LeetCode submission - Find the City With the Smallest Number of Neighbors at a Threshold Distance (cpp)

--- my-folder/problems/find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/solution.cpp
+++ my-folder/problems/find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/solution.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dist(n+1, vector<int>(n, 10001));
-        int res = 0, smallest = n;
-        for(auto &e : edges) {
-            dist[e[0]][e[1]] = dist[e[1]][e[0]] = e[2];
-        }
+        vector<vector<int>> dist(n, vector<int>(n, 10001));
         for(int i = 0; i < n; i++) {
             dist[i][i] = 0;
         }
+        for(vector<int> &e : edges) {
+            dist[e[0]][e[1]] = dist[e[1]][e[0]] = e[2];
+        }
+        
         for(int k = 0; k < n; k++) {
             for(int i = 0; i < n; i++) {
                 for(int j = 0; j < n; j++) {
-                    dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
+        
+        int min_count = INT_MAX;
+        int res = 0;
         for(int i = 0; i < n; i++) {
+            
             int count = 0;
             for(int j = 0; j < n; j++) {
-                if(dist[i][j] <= distanceThreshold) count++;
+                if(dist[i][j] <= distanceThreshold) {
+                    count++;
+                }
             }
-            if(count <= smallest) {
+            
+            if(count <= min_count) {
+                min_count = count;
                 res = i;
-                smallest = count;
             }
         }
+        
         return res;
     }
 };
