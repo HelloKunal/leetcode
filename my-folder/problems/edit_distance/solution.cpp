1208bb3e4406f2df741dee9889d09b2b884bad10
Kunal <hellokunalpawar@gmail.com>
2021-09-06 16:09:46 +0000

    Sync LeetCode submission - Edit Distance (cpp)

--- my-folder/problems/edit_distance/solution.cpp
+++ my-folder/problems/edit_distance/solution.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int len1 = word1.size();
-        int len2 = word2.size();
-        
-        if (len1 == 0)  return len2;
-        if (len2 == 0)  return len1;
-        
-        int dp [len1 + 1][len2 + 1];
-        for (int i = 0; i <= len1; i++)
-        {
-            dp[i][0] = i;
-        }
-        for (int i = 0; i <= len2; i++)
-        {
-            dp[0][i] = i;
+        int len1 = word1.length(), len2 = word2.length();
+        int n = len2;
+        vector<int> dp(n+1);
+        for(int i = 1; i <= n; i++) {
+            dp[i] = i;
         }
         
-        for (int i = 1; i <= len1; i++)
-        {
-            for (int j = 1; j <= len2; j++)
-            {
-                int cost = (word1[i-1] == word2[j-1] ? 0 : 1);
-                int temp = min (dp [i][j - 1] + 1, dp[i - 1][j] + 1);
-                dp [i][j] = min (temp, dp[i - 1][j - 1] + cost);
+        int pre;
+        for(int i = 1; i <= len1; i++) {
+            pre = dp[0];
+            dp[0] = i;
+            for(int j = 1; j <= len2; j++) {
+                int temp = dp[j];
+                if(word1[i-1] == word2[j-1]) dp[j] = pre;
+                else dp[j] = min(pre, min(dp[j], dp[j-1])) + 1;
+                pre = temp;
             }
         }
         
-        return dp[len1][len2];
+        return dp[n];
     }
 };
