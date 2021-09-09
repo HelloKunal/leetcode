e5502b45c347bce33dc70adefd3cd8f7a39805b5
Kunal <hellokunalpawar@gmail.com>
2021-09-09 04:44:15 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> mem;
-    int rec (vector<int> &prices, int pos, int trans, bool bought)
-    {
-        if (pos >= prices.size() || trans == 0)
-            return 0;
-        if (mem[bought][trans][pos] != -1)
-            return mem[bought][trans][pos];
-        
-        int result = rec (prices, pos + 1, trans, bought); // SKIP
-        if (bought)
-        {
-            result = max(result, rec (prices, pos + 1, trans-1, false) + prices[pos]);
-        }
-        else
-        {
-            result = max(result, rec (prices, pos + 1, trans, true) - prices[pos]);            
-        }
-        
-        mem[bought][trans][pos] = result;
-        return result;
-    }
     int maxProfit(vector<int>& prices) {
-         // rec and mem      
-        mem.resize (2, vector<vector<int>> (3, vector<int> (prices.size(), -1)));
-        int res = rec (prices, 0, 2, false);
-        return res;
+        int T_i20 = 0, T_i21 = INT_MIN;
+        int T_i10 = 0, T_i11 = INT_MIN;
+        
+        for(int price : prices) {
+            T_i20 = max(T_i20, T_i21 + price);
+            T_i21 = max(T_i21, T_i10 - price);
+            T_i10 = max(T_i10, T_i11 + price);
+            T_i11 = max(T_i11, -price);
+        }
+        
+        return T_i20;        
     }
 };
