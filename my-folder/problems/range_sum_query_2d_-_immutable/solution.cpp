9c158bd22f6eba2692c1266bd7acabff5524b5d5
Kunal <hellokunalpawar@gmail.com>
2021-09-09 19:43:28 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (cpp)

--- my-folder/problems/range_sum_query_2d_-_immutable/solution.cpp
+++ my-folder/problems/range_sum_query_2d_-_immutable/solution.cpp
@@ -1,19 +1,23 @@
 class NumMatrix {
 public:
-    vector<vector<int>> pSumMatrix;
+    vector<vector<int>> mat; 
     NumMatrix(vector<vector<int>>& matrix) {
         int xAxis = matrix[0].size();
-        int yAxis = matrix.size();
-        pSumMatrix.resize(yAxis+1, vector<int>(xAxis+1, 0));
-        for(int i = 1; i <= yAxis; i++) {
-            for(int j = 1; j <= xAxis; j++) {
-                pSumMatrix[i][j] = pSumMatrix[i-1][j] + pSumMatrix[i][j-1] - pSumMatrix[i-1][j-1] + matrix[i-1][j-1];
+        int yAxis = matrix.size(); 
+        mat.assign(yAxis+1, vector<int>(xAxis+1, 0));
+        
+        for(int y = 1; y <= yAxis; y++) {
+            for(int x = 1; x <= xAxis; x++) {
+                mat[y][x] = matrix[y-1][x-1] + mat[y-1][x] + mat[y][x-1] - mat[y-1][x-1];
             }
-        }        
+        }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return (pSumMatrix[row2+1][col2+1] - pSumMatrix[row2+1][col1] - pSumMatrix[row1][col2+1] + pSumMatrix[row1][col1]);
+        // +1 because dp array start from 0
+        int res = mat[row2+1][col2+1] - mat[row2+1][col1] - mat[row1][col2+1] + mat[row1][col1];
+        
+        return res;
     }
 };
 
