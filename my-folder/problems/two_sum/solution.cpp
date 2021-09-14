0c8f381403f8d79bf1a844f3fe6ed7ded3fcb90d
Kunal <hellokunalpawar@gmail.com>
2021-09-14 13:46:14 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<pair<int, int>> vnums;
-        for (int i = 0; i < n; i++)
-        {
-            vnums.push_back(make_pair(nums[i], i));
+        unordered_map<int, int> um;
+        for(int i = 0; i < nums.size(); i++) {
+            if(um.find(nums[i]) != um.end()) {
+                vector<int> res;
+                res.push_back(um[nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            um[target-nums[i]] = i;
         }
-        sort (vnums.begin(), vnums.end());
-        int start = 0, end = n-1;
-        vector<int> ans;
-        while (start < end)
-        {
-            if (vnums[start].first + vnums[end].first == target)
-            {
-                ans.push_back(vnums[start].second);
-                ans.push_back(vnums[end].second);
-                return ans;
-            }
-            else if (vnums[start].first + vnums[end].first > target)
-            {
-                end--;
-            }
-            else
-            {
-                start++;
-            }
-        }
-        return ans;
+        
+        return {};
     }
 };
