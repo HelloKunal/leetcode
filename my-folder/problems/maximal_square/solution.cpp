6555118cfaf7148cc3e07d1f10a04b510c1b18e0
Kunal <hellokunalpawar@gmail.com>
2021-09-21 17:29:04 +0000

    Sync LeetCode submission - Maximal Square (cpp)

--- my-folder/problems/maximal_square/solution.cpp
+++ my-folder/problems/maximal_square/solution.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int r = matrix.size(), c = matrix[0].size();
-        vector<vector<int>> dp(r, vector<int>(c));
-        int res = 0;
+        int yMax = matrix.size();
+        int xMax = matrix[0].size();
+        int maxSide = 0;
         
-        for(int row = r-1; row >= 0; row--) {
-            for(int col = c-1; col >= 0; col--) {
-                if(row == r-1 || col == c-1) {
-                    dp[row][col] = matrix[row][col] - '0';               
-                } else {
-                    if(matrix[row][col] == '0') {
-                        dp[row][col] = 0;
-                    } else {
-                        dp[row][col] = min(dp[row+1][col+1], min(dp[row+1][col], dp[row][col+1])) + 1;
-                    }
-                }
+        vector<vector<int>> dpArr(yMax, vector<int> (xMax));
+        for(int y = 0; y < yMax; y++) dpArr[y][xMax-1] = matrix[y][xMax-1] - '0', maxSide = max(maxSide, dpArr[y][xMax-1]);
+        for(int x = 0; x < xMax; x++) dpArr[yMax-1][x] = matrix[yMax-1][x] - '0', maxSide = max(maxSide, dpArr[yMax-1][x]);
+        
+        for(int y = yMax-2; y >= 0; y--) {
+            for(int x = xMax-2; x >= 0; x--) {
+                if(matrix[y][x] == '0') dpArr[y][x] = 0;
+                else dpArr[y][x] = min(dpArr[y+1][x+1], min(dpArr[y+1][x], dpArr[y][x+1])) + 1;
                 
-                if(dp[row][col] > res) {
-                    res = dp[row][col];
-                }
+                maxSide = max(maxSide, dpArr[y][x]);
             }
-        }        
+        }
         
-        return res*res;
+        return maxSide*maxSide;
     }
 };
