323106b578027c643cf84dfaef4e0d6e02d6fa81
Kunal <hellokunalpawar@gmail.com>
2021-09-22 09:12:02 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- my-folder/problems/coin_change/solution.cpp
+++ my-folder/problems/coin_change/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int size_c = coins.size();
-        vector<vector<long long int>> diffAmount_to_diffCoinArraySize(size_c+1, vector<long long int>(amount+1));
+        int numCoins = coins.size();
+        vector<vector<long long int>> dpArray(numCoins+1, vector<long long int> (amount+1));
         
-        for(int y = 0; y <= size_c; y++) {
-            diffAmount_to_diffCoinArraySize[y][0] = 0;
-        }
+        for(int i = 0; i <= amount; i++) dpArray[0][i] = INT_MAX-1;
+        for(int y = 0; y <= numCoins; y++) dpArray[y][0] = 0;
         for(int x = 1; x <= amount; x++) {
-            diffAmount_to_diffCoinArraySize[0][x] = INT_MAX-1;
-            // INTMAX - 1 because we check at code for min using arry[i][j] + 1, so no overflow.
+            if(x % coins[0] == 0) dpArray[1][x] = x / coins[0];
+            else dpArray[1][x] = INT_MAX-1;
         }
-        for(int x = 1; x <= amount; x++) {
-            if(x % coins[0] == 0) diffAmount_to_diffCoinArraySize[1][x] = x / coins[0];
-            else diffAmount_to_diffCoinArraySize[1][x] = INT_MAX-1;
-        }
-        for(int y = 2; y <= size_c; y++) {
+        
+        for(int y = 2; y <= numCoins; y++) {
             for(int x = 1; x <= amount; x++) {
-                if(coins[y-1] <= x) diffAmount_to_diffCoinArraySize[y][x] = min(diffAmount_to_diffCoinArraySize[y][x - coins[y-1]] + 1, diffAmount_to_diffCoinArraySize[y-1][x]);
-                else diffAmount_to_diffCoinArraySize[y][x] = diffAmount_to_diffCoinArraySize[y-1][x];
+                
+                if(x >= coins[y-1]) {
+                    dpArray[y][x] = min(1 + dpArray[y][x-coins[y-1]], dpArray[y-1][x]);
+                } else {
+                    dpArray[y][x] = dpArray[y-1][x];
+                }
             }
         }
         
-        if(diffAmount_to_diffCoinArraySize[size_c][amount] > amount) return -1;
-        else return diffAmount_to_diffCoinArraySize[size_c][amount];
+        if(dpArray[numCoins][amount] > amount) return -1;
+        else return dpArray[numCoins][amount];
     }
 };
