a25bd529e8560101cf18d71c1947e2a577a2d7df
Kunal <hellokunalpawar@gmail.com>
2021-09-24 09:59:46 +0000

    Sync LeetCode submission - Burst Balloons (cpp)

--- my-folder/problems/burst_balloons/solution.cpp
+++ my-folder/problems/burst_balloons/solution.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-        int size_n = nums.size();
-        vector<vector<int>> dp(size_n, vector<int>(size_n));
+        int numsSize = nums.size();
+        vector<vector<int>> dpArray(numsSize, vector<int> (numsSize, 0));
         
-        for(int g = 0; g < size_n; g++) {
-            for(int i = 0, j = g; j < size_n; i++, j++) {
+        for(int g = 0; g < numsSize; g++) {
+            for(int i = 0, j = g; j < numsSize; i++, j++) {
                 
-                int max_Ele = INT_MIN;
+                int max_temp = INT_MIN;
                 for(int k = i; k <= j; k++) {
-                    int left = k == i ? 0 : dp[i][k-1];
-                    int right = k == j ? 0 : dp[k+1][j];
-                    int curr = nums[k] * (i == 0 ? 1 : nums[i-1]) * (j == size_n-1 ? 1 : nums[j+1]);
+                    int left = k == i ? 0 : dpArray[i][k-1];
+                    int right = k == j ? 0 : dpArray[k+1][j];
+                    int val = nums[k];
+                    if(i > 0) {
+                        val *= nums[i-1];
+                    }
+                    if(j < numsSize-1) {
+                        val *= nums[j+1];
+                    }
                     
-                    int total = left + right + curr;
-                    max_Ele = max(max_Ele, total);
+                    int total = left + right + val;
+                    max_temp = max(total, max_temp);
                 }
-                dp[i][j] = max_Ele;
+                
+                dpArray[i][j] = max_temp;
             }
-            
         }
         
-        return dp[0][size_n-1];
+        return dpArray[0][numsSize-1];
     }
 };
