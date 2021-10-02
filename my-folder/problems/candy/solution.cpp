9dc847be414b5d737cb20df66ebd73dc40984d06
Kunal <hellokunalpawar@gmail.com>
2021-10-02 04:50:59 +0000

    Sync LeetCode submission - Candy (cpp)

--- my-folder/problems/candy/solution.cpp
+++ my-folder/problems/candy/solution.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings){
-        int numberOfChildren = ratings.size();
-        if (numberOfChildren < 1)
-            return -1;
+    int candy(vector<int>& ratings) {
+        int numChildren = ratings.size();
+        vector<int> candies(numChildren, 1);
         
-        vector<int> candyDist (numberOfChildren, 1);
-        
-        for (int i = 1; i < numberOfChildren; i++)
-        {
-            if (ratings[i] > ratings[i-1])
-                candyDist[i] = candyDist[i-1] + 1;
+        for(int i = 1; i < numChildren; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
         }
         
-        for (int i = numberOfChildren - 1; i > 0; i--)
-        {
-            if (ratings[i-1] > ratings[i])
-                candyDist[i-1] = max (candyDist[i-1], candyDist[i] + 1);
+        for(int i = numChildren-1; i > 0; i--) {
+            if(ratings[i-1] > ratings[i]) {
+                candies[i-1] = max(candies[i-1], candies[i]+1);
+            }
         }
         
-        int numberOfCandies = 0;
-        for (int i = 0; i < numberOfChildren; i++)
-        {
-            numberOfCandies += candyDist[i];
-        }
-        
-        return numberOfCandies;
+        int res = 0;
+        for(int candy : candies) res += candy;
+        return res;
     }
 };
