2114b36d91fde5992702de5c98cb925b0c04a40b
Kunal <hellokunalpawar@gmail.com>
2021-10-17 07:11:47 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (cpp)

--- my-folder/problems/intersection_of_two_linked_lists/solution.cpp
+++ my-folder/problems/intersection_of_two_linked_lists/solution.cpp
@@ -8,48 +8,23 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-         ListNode* ptr1 = head1;
-    ListNode* ptr2 = head2;
-  
-    // If any one of head is NULL i.e
-    // no Intersection Point
-    if (ptr1 == NULL || ptr2 == NULL) {
-  
-        return NULL;
-    }
-  
-    // Traverse through the lists until they
-    // reach Intersection node
-    while (ptr1 != ptr2) {
-  
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
-  
-        // If at any node ptr1 meets ptr2, then it is
-        // intersection node.Return intersection node.
-  
-        if (ptr1 == ptr2) {
-  
-            return ptr1;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *Iter1 = headA, *Iter2 = headB;
+        
+        if(!headA || !headB) return NULL;
+        
+        while(Iter1 != Iter2) {
+            Iter1 = Iter1->next;
+            Iter2 = Iter2->next;
+            
+            if(Iter1 == Iter2) {
+                return Iter1; // that is any
+            }
+            
+            if(!Iter1) Iter1 = headB;
+            if(!Iter2) Iter2 = headA;
         }
-        /* Once both of them go through reassigning,
-        they will be equidistant from the collision point.*/
-  
-        // When ptr1 reaches the end of a list, then
-        // reassign it to the head2.
-        if (ptr1 == NULL) {
-  
-            ptr1 = head2;
-        }
-        // When ptr2 reaches the end of a list, then
-        // redirect it to the head1.
-        if (ptr2 == NULL) {
-  
-            ptr2 = head1;
-        }
-    }
-  
-    return ptr1;
+        
+        return Iter1; // same as above Iter1 == Iter2
     }
 };
