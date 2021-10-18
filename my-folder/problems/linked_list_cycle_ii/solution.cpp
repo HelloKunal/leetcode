358f6f9eed617bfba05f0ae7c46798ef4971b2f7
Kunal <hellokunalpawar@gmail.com>
2021-10-18 04:30:58 +0000

    Sync LeetCode submission - Linked List Cycle II (cpp)

---
+++ my-folder/problems/linked_list_cycle_ii/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head->next) return NULL;
+        
+        ListNode *slow = head, *fast = head, *entry = head;
+        while(fast && fast->next) {
+            fast = (fast->next)->next;
+            slow = slow->next;
+            if(fast == slow) {
+                while(slow != entry) {
+                    slow = slow->next;
+                    entry = entry->next;
+                }
+                
+                return entry;
+            }
+        }
+        
+        return NULL;
+    }
+};
