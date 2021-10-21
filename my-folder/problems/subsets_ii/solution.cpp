3e1d2bad191bb535cabec0ddb72df74ca610120d
Kunal <hellokunalpawar@gmail.com>
2021-10-21 04:38:49 +0000

    Sync LeetCode submission - Subsets II (cpp)

--- my-folder/problems/subsets_ii/solution.cpp
+++ my-folder/problems/subsets_ii/solution.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    void subsetsUtil(vector<int>& A, vector<vector<int> >& res,
-				vector<int>& subset, int index)
-    {
-        res.push_back(subset);
-        for (int i = index; i < A.size(); i++) {
-            if (i != index && A[i] == A[i-1])
-                continue;
-
-            // include the A[i] in subset.
-            subset.push_back(A[i]);
-
-            // move onto the next element.
-            subsetsUtil(A, res, subset, i + 1);
-
-            // exclude the A[i] from subset and triggers
-            // backtracking.
-            subset.pop_back();
+    void subsetsUtil(int curr, int n, vector<int>& arr, vector<int>& nums, vector<vector<int>> &res) {
+        res.push_back(arr);
+        for(int i = curr; i < n; i++) {
+            if(i != curr && nums[i] == nums[i-1]) continue;
+            
+            arr.push_back(nums[i]);
+            subsetsUtil(i+1, n, arr, nums, res);
+            arr.pop_back();
         }
-
-        return;
     }
-
-    // below function returns the subsets of vector A.
-    vector<vector<int> > subsetsWithDup(vector<int>& A)
-    {
-        vector<int> subset;
-        vector<vector<int> > res;
-        
-        sort(A.begin(), A.end());
-
-        // keeps track of current element in vector A;
-        int index = 0;
-        subsetsUtil(A, res, subset, index);
-
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        subsetsUtil(0, nums.size(), temp, nums, res);
         return res;
     }
 };
