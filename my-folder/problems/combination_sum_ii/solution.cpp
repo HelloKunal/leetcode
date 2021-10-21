58a3d80869dd10f96cd123de4286a579cec41f96
Kunal <hellokunalpawar@gmail.com>
2021-10-21 08:59:59 +0000

    Sync LeetCode submission - Combination Sum II (cpp)

--- my-folder/problems/combination_sum_ii/solution.cpp
+++ my-folder/problems/combination_sum_ii/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    void rec (int index, int target, vector<int> &candidates, vector<vector<int>> &res, vector<int> &temp_res)
-    {
-        if (target == 0)
-        {
-            res.push_back (temp_res);
+    void csUtil(int curr, vector<int>& candidates, vector<vector<int>>& res, vector<int>& tempRes, int target) {
+        if(!target) {
+            res.push_back(tempRes);
             return;
         }
         
-        for (int i = index, y = candidates.size(); i < y; i++)
-        {
-            if (i > index && candidates[i] == candidates[i-1]) continue;
-            if (candidates[i] > target) break;
-                
-            temp_res.push_back(candidates[i]);
-            rec (i+1, target-candidates[i], candidates, res, temp_res);
-            temp_res.pop_back();                     
+        for(int i = curr; i < candidates.size(); i++) {
+            if(i != curr && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break; // iincreasing order, later elements will always be bigger
+            
+            tempRes.push_back(candidates[i]);
+            csUtil(i+1, candidates, res, tempRes, target-candidates[i]);
+            tempRes.pop_back();
         }
-    }
-    
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> temp_res;
-        sort (candidates.begin(), candidates.end());
         
-        rec (0, target, candidates, res, temp_res);        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> tempRes;
+        vector<vector<int>> res;
+        csUtil(0, candidates, res, tempRes, target);
         return res;
     }
 };
