76e73a8adff14084c52b7e43862e8c3c09def492
Kunal <hellokunalpawar@gmail.com>
2021-10-25 10:05:43 +0000

    Sync LeetCode submission - Partition Labels (cpp)

--- my-folder/problems/partition_labels/solution.cpp
+++ my-folder/problems/partition_labels/solution.cpp
@@ -1,31 +1,25 @@
-// naive is n2: look for first and last occurance of each letter. make intervaks and merge overlapping intervals.
-
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int sLength = s.length();
-        vector<int> res;
-        int lastLetterOcc [26];
-        
-        for (int i = 0; i < sLength; i++)
-        {
-            lastLetterOcc [s[i] - 'a'] = i;
+        int stringLen = s.length();
+        vector<int> sizeLabels;
+        int lastOccurLetters[26];
+        for(int i = 0; i < stringLen; i++) {
+            lastOccurLetters[s[i] - 'a'] = i;
         }
         
         int i = 0;
-        while (i < sLength)
-        {
-            int end = lastLetterOcc[s[i] - 'a'];
+        while(i < stringLen) {
+            int lOL = lastOccurLetters[s[i] - 'a'];
             int j = i;
-            while (j != end)
-            {
-                end = max (end, lastLetterOcc[s[j++] - 'a']);
+            while(j != lOL) {
+                lOL = max(lOL, lastOccurLetters[s[j++] - 'a']);
             }
             
-            res.push_back (j - i + 1);
-            i = j + 1;
+            sizeLabels.push_back(j-i+1);
+            i = j+1;
         }
         
-        return res;
+        return sizeLabels;
     }
 };
