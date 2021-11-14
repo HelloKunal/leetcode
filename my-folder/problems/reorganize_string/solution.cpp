0e424b4a5bd5aa65ce3e3c052fc59665f7aabed9
Kunal <hellokunalpawar@gmail.com>
2021-11-14 12:07:17 +0000

    Sync LeetCode submission - Reorganize String (cpp)

--- my-folder/problems/reorganize_string/solution.cpp
+++ my-folder/problems/reorganize_string/solution.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    string reorganizeString(string S) {
-    string res="";
-        unordered_map<char,int> mp;
-        priority_queue<pair<int,char>>pq;
+    string reorganizeString(string s) {
+        if(s.length() == 1) return s;
         
-        for(auto s: S)
-            mp[s]+=1;
-        
-        for(auto m: mp)
-            pq.push(make_pair(m.second,m.first));
-        
-        while(pq.size()>1){
-            auto top1= pq.top();
-            pq.pop();
-            auto top2 = pq.top();
-            pq.pop();
-            
-            res+=top1.second;
-            res+=top2.second;
-            
-            top1.first -=1;
-            top2.first -= 1;
-            
-            if(top1.first > 0)
-                pq.push(top1);
-            
-            if(top2.first > 0)
-                pq.push(top2);
+        vector<int> charFreqArr(26, 0);
+        priority_queue<pair<int, char>> maxHeap;
+        for(char c : s) charFreqArr[c - 'a']++;
+        for(int i = 0; i <= 25;  i++) {
+            if(charFreqArr[i]) {
+                maxHeap.push({charFreqArr[i], (char)(i+'a')});
+            }
         }
         
-        if(!pq.empty()){
-            if(pq.top().first > 1)
-                return "";
-            
-            else
-                res+=pq.top().second;
+        string res = "";
+        while(maxHeap.size() >= 2) {
+            auto a = maxHeap.top(); maxHeap.pop();
+            auto b = maxHeap.top(); maxHeap.pop();
+            res += a.second;
+            res += b.second;
+            if(--a.first) maxHeap.push(a);
+            if(--b.first) maxHeap.push(b);
+        }
+        
+        if(maxHeap.size() == 1) {
+            auto a = maxHeap.top();
+            if(a.first >= 2) return "";
+            else res += a.second;
         }
         
         return res;
