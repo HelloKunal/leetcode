1623c8cb67c73dce01d2e5b7c62af48637394691
Kunal <hellokunalpawar@gmail.com>
2021-11-14 12:19:56 +0000

    Sync LeetCode submission - Car Pooling (cpp)

--- my-folder/problems/car_pooling/solution.cpp
+++ my-folder/problems/car_pooling/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        map<int, int> map;
-        for (int i = 0;i < trips.size();++i) {
-            map[trips[i][1]] += trips[i][0]; //filled seats increase when we pick up passengers
-            map[trips[i][2]] -= trips[i][0]; //filled seats decrease when we drop off the passengers
+        vector<int> countAtMom(1001);
+        for(auto trip : trips) {
+            countAtMom[trip[1]] += trip[0];
+            countAtMom[trip[2]] -= trip[0];
         }
-        for (auto it = map.begin(); it != map.end();it++) { //map is sorted on keys( key is time instant)
-            capacity -= it->second;
-            if (capacity < 0) { //if at any time instant, vacant seats < no of passengers to pick up, return false
-                return false;
-            }
+        
+        int count = 0;
+        for(int i : countAtMom) {
+            count += i;
+            if(count > capacity) return false;
         }
         return true;
     }
