000d9ba5ace881f6909fc58434154f415ca09e84
Kunal <hellokunalpawar@gmail.com>
2021-12-27 06:42:12 +0000

    Sync LeetCode submission - Distinct Subsequences (cpp)

--- my-folder/problems/distinct_subsequences/solution.cpp
+++ my-folder/problems/distinct_subsequences/solution.cpp
@@ -1,27 +1,26 @@
+#define ll unsigned long long
+
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int len_s = s.length();
-        int len_t = t.length();
+        int sLen = s.length(), tLen = t.length();
+        vector<vector<ll>> dp(tLen+1, vector<ll> (sLen+1));
         
-        vector<vector<unsigned long long>> dp(len_t+1, vector<unsigned long long>(len_s+1, 0));
-        for(int i = 1; i <= len_s; i++) {
-            if(t[0] == s[i-1]) {
-                dp[1][i] = dp[1][i-1] + 1;
-            } else {
-                dp[1][i] = dp[1][i-1];
-            }
-        }
-        for(int i = 2; i <= len_t; i++) {
-            for(int j = 1; j <= len_s; j++) {
-                if(t[i-1] == s[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1]; 
-                } else {
+        // empty string cant have another string as subsequence
+        for(int i = 0; i <= tLen; i++) dp[i][0] = 0;
+        // empty string is subsequence after all
+        for(int i = 0; i <= sLen; i++) dp[0][i] = 1;
+        
+        for(int i = 1; i <= tLen; i++) {
+            for(int j = 1; j <= sLen; j++) {
+                if(t[i-1] != s[j-1]) {
                     dp[i][j] = dp[i][j-1];
+                } else {
+                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
                 }
             }
-        }
+        }        
         
-        return dp[len_t][len_s];
+        return dp[tLen][sLen];
     }
 };
