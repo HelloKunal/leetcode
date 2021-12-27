06612678913cf1e374d96ca597293b6c87b821f7
Kunal <hellokunalpawar@gmail.com>
2021-12-27 08:59:51 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int T_ik0 = 0, T_ik1 = INT_MIN;
-        int T_ik0old1 = 0, T_ik0_old2 = 0;
-        
+        int T_ik0_old, T_ik0_pre = 0;
         for(int price : prices) {
-            T_ik0old1 = T_ik0;
+            T_ik0_old = T_ik0;
             T_ik0 = max(T_ik0, T_ik1 + price);
-            T_ik1 = max(T_ik1, T_ik0_old2 - price);
-            T_ik0_old2 = T_ik0old1;
+            T_ik1 = max(T_ik1, T_ik0_pre - price);
+            T_ik0_pre = T_ik0_old;
         }
         
-        return T_ik0;
+        return T_ik0;        
     }
 };
