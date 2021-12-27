27ada24f576cd2d0430b699b4ae9659b93617c54
Kunal <hellokunalpawar@gmail.com>
2021-12-27 09:41:52 +0000

    Sync LeetCode submission - Russian Doll Envelopes (cpp)

--- my-folder/problems/russian_doll_envelopes/solution.cpp
+++ my-folder/problems/russian_doll_envelopes/solution.cpp
@@ -3,26 +3,22 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end());
         
-        vector<int> dp(envelopes.size());
-        int omax = 0;
-        for(int i = 0; i < envelopes.size(); i++) {
+        int n = envelopes.size();
+        vector<int> dpLIS(n);
+        int oMax =0;
+        for(int i = 0; i < n; i++) {
             
             int tmax = 0;
             for(int j = 0; j < i; j++) {
-                if(envelopes[j][1] < envelopes[i][1] && envelopes[j][0] < envelopes[i][0]) {
-                    if(dp[j] > tmax) {
-                        tmax = dp[j];
-                    }
+                if(envelopes[i][1] > envelopes[j][1] && envelopes[i][0] > envelopes[j][0]) {
+                    tmax = max(tmax, dpLIS[j]);
                 }
             }
             
-            dp[i] = tmax + 1;
-            if(dp[i] > omax) {
-                omax = dp[i];
-            }
+            dpLIS[i] = tmax + 1;
+            oMax = max(oMax, dpLIS[i]);
         }
         
-        return omax;    
-        
+        return oMax;
     }
 };
