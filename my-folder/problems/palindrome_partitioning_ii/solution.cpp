ea383decdce8d2e5a6de9f8ccdfc6e552ce011c7
Kunal <hellokunalpawar@gmail.com>
2021-12-27 07:22:55 +0000

    Sync LeetCode submission - Palindrome Partitioning II (cpp)

--- my-folder/problems/palindrome_partitioning_ii/solution.cpp
+++ my-folder/problems/palindrome_partitioning_ii/solution.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int minCut(string s) {
-        vector<vector<bool>> gapDP(s.length(), vector<bool>(s.length()));
+        int n = s.length();
+        vector<vector<bool>> gapDP(n, vector<bool> (n));
         
-        for(int g = 0; g < s.length(); g++) {
-            for(int i = 0, j = g; j < s.length(); i++, j++) {
-                if(g == 0) {
-                    gapDP[i][j] = true;
-                } else if(g == 1) {
-                    gapDP[i][j] = s[i] == s[j];
-                } else {
-                    if(s[i] == s[j] && gapDP[i+1][j-1] == true) {
-                        gapDP[i][j] = true;
-                    } else {
-                        gapDP[i][j] = false;
-                    }
+        for(int g = 0; g < n; g++) {
+            for(int i = 0, j = g; j < n; i++, j++) {
+                if(g == 0) gapDP[i][j] = true;
+                else if(g == 1) gapDP[i][j] = s[i] == s[j];
+                else {
+                    if(s[i] == s[j] && gapDP[i+1][j-1]) gapDP[i][j] = true;
+                    else gapDP[i][j] = false;
                 }
             }
         }
         
-        vector<int> DP(s.length());
-        for(int j = 1; j < s.length(); j++) {
+        vector<int> DP(n);
+        for(int j = 1; j < n; j++) {
             if(gapDP[0][j]) {
+                // start to j whole palindrome
                 DP[j] = 0;
-            } else {                
-                int min = INT_MAX;
+            } else {
+                // palindrome in between
+                int mi = INT_MAX;
                 for(int i = j; i >= 1; i--) {
                     if(gapDP[i][j]) {
-                        if(DP[i-1] < min) {
-                            min = DP[i-1];
-                        }
+                        mi = min(mi, DP[i-1]);
                     }
                 }
-
-                DP[j] = min+1;
+                
+                DP[j] = mi+1;                 
             }
         }
         
-        return DP[s.length()-1];
+        return DP[n-1];
     }
 };
