300ab0854d37a8cf541e27f494c55f6b43113ff0
Kunal <hellokunalpawar@gmail.com>
2021-12-27 10:58:10 +0000

    Sync LeetCode submission - Largest Divisible Subset (cpp)

--- my-folder/problems/largest_divisible_subset/solution.cpp
+++ my-folder/problems/largest_divisible_subset/solution.cpp
@@ -4,33 +4,28 @@ public:
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        if(n == 1) return {nums[0]};
-        vector<int> dp(n);
-        dp[0] = 1;
+        vector<int> dpLIS(n);
         int omax = 0;
-        for(int i = 1; i < n; i++) {
+        for(int i = 0; i < n; i++) {
+            
             int tmax = 0;
             for(int j = 0; j < i; j++) {
                 if(nums[i] % nums[j] == 0) {
-                    if(dp[j] > tmax) {
-                        tmax = dp[j];
-                    }
+                    tmax = max(tmax, dpLIS[j]);
                 }
             }
-            dp[i] = tmax + 1;
             
-            if(dp[i] > omax) {
-                omax = dp[i];
-            }
+            dpLIS[i] = tmax + 1;
+            omax = max(omax, dpLIS[i]);
         }
         
         vector<int> res;
         int prev = -1;
         for(int i = n-1; i >= 0; i--) {
-            if(dp[i] == omax && (prev%nums[i] == 0 || prev == -1)) {
-                res.push_back(nums[i]);
-                omax--;
+            if(dpLIS[i] == omax && (prev % nums[i] == 0 || prev == -1)) {
                 prev = nums[i];
+                omax--;
+                res.push_back(nums[i]);
             }
         }
         
