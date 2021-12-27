f0d5fad34306c5cdffcf9dc82ada69d090eecc0f
Kunal <hellokunalpawar@gmail.com>
2021-12-27 11:09:52 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- my-folder/problems/partition_equal_subset_sum/solution.cpp
+++ my-folder/problems/partition_equal_subset_sum/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> memo;
-    bool subsetSumUtil(int curr, int n, int sum, int target, vector<int>& nums) {
-        if(curr == n) {
-            if(sum == target) return true;
-            else return false;
-        }
-        if(memo[curr][sum] != -1) return memo[curr][sum];
+    bool rec(vector<int>& nums, int i, int target) {
         
-        return memo[curr][sum] = subsetSumUtil(curr+1, n, sum+nums[curr], target, nums) || 
-        subsetSumUtil(curr+1, n, sum, target, nums);
+        if(target <= 0) {
+            if(!target) return true; 
+            return false;
+        }
+        
+        if(i >= nums.size()) return false;
+        
+        if(memo[i][target] != -1) return memo[i][target];
+        
+        return memo[i][target] = rec(nums, i+1, target - nums[i]) || rec(nums, i+1, target);
     }
-    
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int i : nums) sum += i;
+        for(int a : nums) sum += a;
+        
         if(sum % 2 == 1) return false;
         else {
-            int newsum = sum/2;
-            memo.assign(nums.size(), vector<int> (sum+1, -1));
-            return subsetSumUtil(0, nums.size(), 0, newsum, nums);
+            sum /= 2;
+            memo.assign(nums.size()+1, vector<int> (sum+1, -1));
+            return rec(nums, 0, sum);
         }
+        
+        
     }
 };
