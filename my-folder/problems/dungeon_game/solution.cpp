98842189389a49353a5d0345d2c8e4266afb5a31
Kunal <hellokunalpawar@gmail.com>
2021-12-27 07:33:06 +0000

    Sync LeetCode submission - Dungeon Game (cpp)

--- my-folder/problems/dungeon_game/solution.cpp
+++ my-folder/problems/dungeon_game/solution.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int rows = dungeon.size();
-        int cols = dungeon[0].size();
-        vector<vector<int>> dp(rows, vector<int>(cols));
+        int yAxis = dungeon.size();
+        int xAxis = dungeon[0].size();
+        vector<vector<int>> dpDung(yAxis, vector<int> (xAxis));
         
-        for(int i = rows-1; i >= 0; i--) {
-            for(int j = cols-1; j >= 0; j--) {
-                
-                if(i == rows-1 && j == cols-1) {
-                    dp[i][j] = min(0, dungeon[i][j]);
-                } else if(i == rows-1) {
-                    dp[i][j] = min(0, dungeon[i][j] + dp[i][j+1]);
-                } else if(j == cols-1) {
-                    dp[i][j] = min(0, dungeon[i][j] + dp[i+1][j]);
-                } else {
-                    dp[i][j] = min(0, dungeon[i][j] + max(dp[i+1][j], dp[i][j+1]));
-                }
+        dpDung[yAxis-1][xAxis-1] = min(0, dungeon[yAxis-1][xAxis-1]);
+        for(int i = yAxis-2; i >= 0; i--) {
+            dpDung[i][xAxis-1] = min(0, dpDung[i+1][xAxis-1] + dungeon[i][xAxis-1]);
+        }
+        for(int i = xAxis-2; i >= 0; i--) {
+            dpDung[yAxis-1][i] = min(0, dpDung[yAxis-1][i+1] + dungeon[yAxis-1][i]);
+        }
+        for(int i = yAxis-2; i >= 0; i--) {
+            for(int j = xAxis-2; j >= 0; j--) {
+                dpDung[i][j] = min(0, max(dpDung[i+1][j], dpDung[i][j+1]) + dungeon[i][j]);
             }
         }
         
-        return abs(dp[0][0]) + 1;
+        return abs(dpDung[0][0])+1;
     }
 };
