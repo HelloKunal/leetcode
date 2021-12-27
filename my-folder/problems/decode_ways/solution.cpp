18a2f3ccfd7ee38f15cd058b094744f80946f60a
Kunal <hellokunalpawar@gmail.com>
2021-12-27 06:11:12 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- my-folder/problems/decode_ways/solution.cpp
+++ my-folder/problems/decode_ways/solution.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int stringLen = s.length();
-        vector<int> dpArray(stringLen+1);
+        int sLen = s.length();
+        vector<int> dp(sLen+1);
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
         
-        dpArray[0] = 1;
-        dpArray[1] = (s[0] == '0') ? 0 : 1;
-        
-        for(int i = 2; i <= stringLen; i++) {
+        for(int i = 2; i <= sLen; i++) {
             int oneDigit = stoi(s.substr(i-1, 1));
             int twoDigit = stoi(s.substr(i-2, 2));
             
             if(oneDigit >= 1) {
-                dpArray[i] += dpArray[i-1];
+                dp[i] += dp[i-1];
             }
             
             if(twoDigit >= 10 && twoDigit <= 26) {
-                dpArray[i] += dpArray[i-2];
+                dp[i] += dp[i-2];
             }
-        }
+        }        
         
-        return dpArray[stringLen];
+        return dp[sLen];
     }
 };
