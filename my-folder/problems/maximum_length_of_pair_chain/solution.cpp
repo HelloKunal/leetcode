720537ea3736ff8902177ced4a25d840904fc953
Kunal <hellokunalpawar@gmail.com>
2021-12-29 07:39:34 +0000

    Sync LeetCode submission - Maximum Length of Pair Chain (cpp)

--- my-folder/problems/maximum_length_of_pair_chain/solution.cpp
+++ my-folder/problems/maximum_length_of_pair_chain/solution.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        int numPairs = pairs.size();
         sort(pairs.begin(), pairs.end());
-        vector<int> dp(numPairs);
-        
         int omax = 0;
-        for(int i = 0; i < numPairs; i++) {
-            int t = 0;
+        int n = pairs.size();
+        vector<int> DP(n);
+        for(int i = 0; i < n; i++) {
+            int tmax = 0;
             for(int j = 0; j < i; j++) {
                 if(pairs[j][1] < pairs[i][0]) {
-                    if(dp[j] > t) {
-                        t = dp[j];
-                    }
+                    tmax = max(tmax, DP[j]);
                 }
             }
             
-            dp[i] = t + 1;
-            omax = max(dp[i], omax);
+            DP[i] = tmax + 1;
+            omax = max(omax, DP[i]);
         }
+        
         
         return omax;
     }
