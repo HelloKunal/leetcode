177c24686be002b6396471c387f537b66b96513f
Kunal <hellokunalpawar@gmail.com>
2021-12-29 05:21:11 +0000

    Sync LeetCode submission - Coin Change 2 (cpp)

--- my-folder/problems/coin_change_2/solution.cpp
+++ my-folder/problems/coin_change_2/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int numCoins = coins.size();
-        vector<vector<int>> dp (numCoins+1, vector<int>(amount+1));
-        for(int x = 0; x <= amount; x++) dp[0][x] = 0;
-        for(int y = 0; y <= numCoins; y++) dp[y][0] = 1;
-        for(int y = 1; y <= numCoins; y++) {
-            for(int x = 1; x <= amount; x++) {
-                if(coins[y-1] <= x) {
-                    dp[y][x] = dp[y-1][x] + dp[y][x - coins[y-1]];
-                } else {
-                    dp[y][x] = dp[y-1][x];
+        int n = coins.size();
+        vector<vector<int>> DP(amount+1, vector<int> (n+1));
+        
+        for(int i = 0; i <= amount; i++) DP[i][0] = 0;
+        for(int i = 0; i <= n; i++) DP[0][i] = 1;
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(coins[j-1] > i) DP[i][j] = DP[i][j-1];
+                else {
+                    DP[i][j] = DP[i][j-1] + DP[i - coins[j-1]][j];
                 }
             }
         }
         
-        return dp[numCoins][amount];
+        return DP[amount][n];
     }
 };
