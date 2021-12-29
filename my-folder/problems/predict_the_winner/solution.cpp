388499804457ed5ca47508e44a465d554334be78
Kunal <hellokunalpawar@gmail.com>
2021-12-29 04:10:23 +0000

    Sync LeetCode submission - Predict the Winner (cpp)

--- my-folder/problems/predict_the_winner/solution.cpp
+++ my-folder/problems/predict_the_winner/solution.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
+        int sum = 0;
+        for(int n : nums) sum += n;
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n));
-        for (int i = 0; i < n; i++) dp[i][i] = nums[i];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j+i < n; j++) {
-                dp[j][j+i] = max(nums[j+i]-dp[j][j+i-1], nums[j]-dp[j+1][j+i]);
+        vector<vector<int>> dpArr(n, vector<int> (n));
+        
+        for(int g = 0; g < n; g++) {
+            for(int i = 0, j = g; j < n; i++, j++) {
+                if(g == 0) {
+                    dpArr[i][j] = nums[i];
+                } else if(g == 1) {
+                    dpArr[i][j] = max(dpArr[i][j-1], dpArr[i+1][j]);
+                } else {
+                    int val1 = nums[j] + min(dpArr[i][j-2], dpArr[i+1][j-1]);
+                    int val2 = nums[i] + min(dpArr[i+2][j], dpArr[i+1][j-1]);
+                    
+                    dpArr[i][j] = max(val1, val2);
+                }
             }
         }
-        return dp[0][n-1] >= 0;
+
+        return dpArr[0][n-1] >= (sum - dpArr[0][n-1]);
     }
 };
