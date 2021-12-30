b2767166ba575851f5bd340db2fe0b1ff7cc4132
Kunal <hellokunalpawar@gmail.com>
2021-12-30 03:36:19 +0000

    Sync LeetCode submission - Minimum ASCII Delete Sum for Two Strings (cpp)

--- my-folder/problems/minimum_ascii_delete_sum_for_two_strings/solution.cpp
+++ my-folder/problems/minimum_ascii_delete_sum_for_two_strings/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        vector<vector<int>> dpArr(s1.length()+1, vector<int> (s2.length()+1));
+        vector<vector<int>> DP(s1.length()+1, vector<int> (s2.length()+1));
         for(int i = s1.length(); i >= 0; i--) {
             for(int j = s2.length(); j >= 0; j--) {
-                if(i == s1.length() && j == s2.length()) {
-                    dpArr[i][j] = 0;
-                } else if(i == s1.length()) {
-                    dpArr[i][j] = (int)s2[j] + dpArr[i][j+1];
-                } else if(j == s2.length()) {
-                    dpArr[i][j] = (int)s1[i] + dpArr[i+1][j];
-                } else {
-                    if(s1[i] == s2[j]) {
-                        dpArr[i][j] = dpArr[i+1][j+1];
-                    } else {
-                        dpArr[i][j] = min((int)s1[i] + dpArr[i+1][j], (int)s2[j] + dpArr[i][j+1]);
-                    }                    
+                if(i == s1.length() && j == s2.length()) DP[i][j] = 0;
+                else if(i == s1.length()) DP[i][j] = s2[j] + DP[i][j+1];
+                else if(j == s2.length()) DP[i][j] = s1[i] + DP[i+1][j];
+                else {
+                    if(s1[i] == s2[j]) DP[i][j] = DP[i+1][j+1];
+                    else DP[i][j] = min(s1[i] + DP[i+1][j], s2[j] + DP[i][j+1]);
                 }
             }
         }
         
-        return dpArr[0][0];
+        return DP[0][0];
     }
 };
