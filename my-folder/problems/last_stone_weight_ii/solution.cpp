c891167e75f2d382648b1b364a22b8f252e806f4
Kunal <hellokunalpawar@gmail.com>
2022-01-04 06:39:56 +0000

    Sync LeetCode submission - Last Stone Weight II (cpp)

--- my-folder/problems/last_stone_weight_ii/solution.cpp
+++ my-folder/problems/last_stone_weight_ii/solution.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        bitset<1501> dp = {1};
-        int sumA = 0;
-        for(int stone : stones) {
-            sumA += stone;
-            for(int i = min(1500, sumA); i >= stone; i--) {
-                dp[i] = dp[i] + dp[i-stone];
+        int n = stones.size();
+        int s = 0;
+        for(int i : stones) s += i;
+        vector<vector<bool>> knapsackDP(n+1, vector<bool> ((s/2)+1, false));
+        knapsackDP[0][0] = true;
+        for(int i = 0; i < n; i++) {
+            knapsackDP[i][0] = true;
+        }
+        
+        int omax = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= s/2; j++) {
+                knapsackDP[i][j] = knapsackDP[i-1][j];
+                if(j >= stones[i-1]) {
+                    knapsackDP[i][j] = knapsackDP[i-1][j] || knapsackDP[i-1][j-stones[i-1]];
+                }
+                if(knapsackDP[i][j] == true) {
+                    omax = max(omax, j);
+                }
             }
         }
-        for(int i = sumA/2; i >= 0; i--) {
-            if(dp[i]) return sumA - i - i;
-        }
-        return 0;
+        
+        return s-(2*omax);
     }
 };
