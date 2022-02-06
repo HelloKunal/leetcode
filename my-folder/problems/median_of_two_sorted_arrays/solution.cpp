9a27cd330c441a989f2f9e3e6efb9e2b26751bc2
Kunal <hellokunalpawar@gmail.com>
2022-02-06 10:33:21 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (cpp)

--- my-folder/problems/median_of_two_sorted_arrays/solution.cpp
+++ my-folder/problems/median_of_two_sorted_arrays/solution.cpp
@@ -1,37 +1,25 @@
-
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       if (nums2.size() < nums1.size())
-            return findMedianSortedArrays(nums2, nums1);
-
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int low = 0, high = n1;
-
-        while (low <= high)
-        {
-            int cut1 = (low + high) >> 1;
-            int cut2 = ((n1 + n2 + 1) / 2) - cut1;
-
-            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
-            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
-            int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
-            int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1)
-            {
-                if ((n1 + n2) % 2 == 0)
-                    return (max (l1, l2) + min (r1, r2)) / 2.0;
-                else
-                    return max (l1, l2);
-            }
-            else if (l1 > r2)
-                high = cut1 - 1;
-            else
-                low = cut1 + 1;
+        int N1 = nums1.size();
+        int N2 = nums2.size();
+        
+        if(N1 < N2) return findMedianSortedArrays(nums2, nums1);
+        int lo = 0, hi = N2*2;
+        while(lo <= hi) {
+            int mid2 = (lo + hi) / 2;
+            int mid1 = N1 + N2 - mid2;
+            
+            double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1-1)/2];
+            double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2-1)/2];
+            double R1 = (mid1 == N1*2) ? INT_MAX : nums1[(mid1)/2];
+            double R2 = (mid2 == N2*2) ? INT_MAX : nums2[(mid2)/2];
+            
+            if(L1 > R2) lo = mid2 + 1;
+            else if(L2 > R1) hi = mid2 - 1;
+            else return (max(L1, L2) + min(R1, R2)) / 2;
         }
-
-        return 0.0;
+        
+        return -1;
     }
 };
