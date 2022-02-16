9dd5444274887056ed2becd0d260dfba0ad814e5
Kunal <hellokunalpawar@gmail.com>
2022-02-16 13:49:04 +0000

    Sync LeetCode submission - Implement strStr() (cpp)

--- my-folder/problems/implement_strstr()/solution.cpp
+++ my-folder/problems/implement_strstr()/solution.cpp
@@ -1,52 +1,31 @@
 class Solution {
 public:
-    vector<int> lpsarray (string needle, int n)
-    {
-        vector<int> temp (n, 0);
-        int len = 0;
-        for (int i = 1; i < n;)
-        {
-            if (needle[i] == needle[len])
-            {
-                temp[i++] = ++len;
-            }
-            else if (len)
-            {
-                len = temp[len - 1];
-            }
-            else
-            {
-                temp[i++] = 0;
-            } 
+    int strStr(string haystack, string needle) {
+        int nedHash = 0;
+        if(needle == "") return 0;
+        if(haystack.length() < needle.length()) return -1;
+        for(char c : needle) {
+            nedHash += c - 'a';
         }
         
-        return temp;
-    }
-    int strStr(string haystack, string needle) {
-        int m = haystack.size();
-        int n = needle.size();
-        
-        if (n == 0)
-            return 0;
-        
-        vector<int> lps = lpsarray(needle, n);
-        for (int i = 0, j = 0; i < m;)
-        {
-            if (haystack[i] == needle[j])
-            {
-                i++;
-                j++;
-            }
-            
-            if (j == n)
-            {
-                return i - j;
-                j = lps[j - 1];
-            }
-            
-            if (i < m && haystack[i] != needle[j])
-            {
-                j ? j = lps[j - 1] : i++;
+        int hayHash = 0;
+        for(int i = 0; i < haystack.size(); i++) {
+            hayHash += (haystack[i] - 'a');
+            if(i >= needle.size() - 1) {
+                if(hayHash == nedHash) {
+                    // return 1;
+                    bool pos = true;
+                    for(int j = 0; j < needle.size(); j++) {
+                        if(haystack[i + j - needle.size() + 1] != needle[j]) {
+                            pos = false;
+                            break;
+                        }
+                    }
+                    
+                    if(pos) return i - needle.size() + 1;
+                }
+                
+                hayHash -= (haystack[i - needle.size() + 1] - 'a');
             }
         }
         
