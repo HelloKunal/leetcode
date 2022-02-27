83e2f21ad6bee785c9e2226739c3601999e49c71
Kunal <hellokunalpawar@gmail.com>
2022-02-27 17:47:26 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- my-folder/problems/number_of_islands/solution.cpp
+++ my-folder/problems/number_of_islands/solution.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    void dfs(int x, int y, int xAxis, int yAxis, vector<vector<char>> &grid) {
-        if(x < 0 || x > xAxis-1 || y < 0 || y > yAxis-1) return;
-        if(grid[y][x] == '0' || grid[y][x] == '2') return;
-        grid[y][x] = '2';
-        
-        dfs(x+1, y, xAxis, yAxis, grid);
-        dfs(x-1, y, xAxis, yAxis, grid);
-        dfs(x, y+1, xAxis, yAxis, grid);
-        dfs(x, y-1, xAxis, yAxis, grid);
+    int dxy[5] = {0, 1, 0, -1, 0};
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        grid[i][j] = '2';
+        for(int k = 0; k < 4; k++) {
+            int nY = i + dxy[k];
+            int nX = j + dxy[k+1];
+            
+            if(nX >= grid[0].size() || nY >= grid.size() || nX < 0 || nY < 0) continue;
+            if(grid[nY][nX] == '1') dfs(grid, nY, nX);
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int yAxis = grid.size();
-        int xAxis = grid[0].size();
-        
-        int res = 0;
-        for(int x = 0; x < xAxis; x++) {
-            for(int y = 0; y < yAxis; y++) {
+        int count = 0;
+        for(int y = 0; y < grid.size(); y++) {
+            for(int x = 0; x < grid[0].size(); x++) {
                 if(grid[y][x] == '1') {
-                    dfs(x, y, xAxis, yAxis, grid);
-                    res++;
+                    dfs(grid, y, x);
+                    count++;
                 }
             }
         }
-        return res;
+        
+        return count;
     }
 };
