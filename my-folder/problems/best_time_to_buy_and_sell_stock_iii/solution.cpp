69d9a84b7808f6ed131a9ad15762d4cf4dcdd61c
Kunal <hellokunalpawar@gmail.com>
2022-03-26 08:24:25 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int T_i20 = 0, T_i21 = INT_MIN;
-        int T_i10 = 0, T_i11 = INT_MIN;
-        
+        int T_i01 = 0, T_i11 = INT_MIN;
+        int T_j01 = 0, T_j11 = INT_MIN;
         for(int price : prices) {
-            T_i20 = max(T_i20, T_i21 + price);
-            T_i21 = max(T_i21, T_i10 - price);
-            T_i10 = max(T_i10, T_i11 + price);
+            T_j01 = max(T_j01, T_j11 + price);
+            T_j11 = max(T_j11, T_i01 - price);
+            T_i01 = max(T_i01, T_i11 + price);
             T_i11 = max(T_i11, -price);
         }
         
-        return T_i20;        
+        return T_j01;
     }
 };
