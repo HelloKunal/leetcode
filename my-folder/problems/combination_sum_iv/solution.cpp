7b9131eeff59b7b2029db773f49d105ddee1463f
Kunal <hellokunalpawar@gmail.com>
2022-03-27 04:58:29 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

--- my-folder/problems/combination_sum_iv/solution.cpp
+++ my-folder/problems/combination_sum_iv/solution.cpp
@@ -3,13 +3,15 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         vector<long long> dp(target+1);
         dp[0] = 1;
-        for(int i = 1; i <= target; i++) {            
+        for(int i = 1; i <= target; i++) {
             dp[i] = 0;
-            for(int n : nums) {
-                if(i >= n) dp[i] += dp[i-n];
+            for(int j : nums) {
+                if(i >= j) dp[i] += dp[i-j];
                 if(dp[i] > INT_MAX) break;
             }
+            
         }
+        
         return dp[target];
     }
 };
