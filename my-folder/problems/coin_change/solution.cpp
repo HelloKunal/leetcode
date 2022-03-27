85b7074923bf27cdd394d0bb16789af18eeb9a4a
Kunal <hellokunalpawar@gmail.com>
2022-03-27 04:45:51 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- my-folder/problems/coin_change/solution.cpp
+++ my-folder/problems/coin_change/solution.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dpArr(amount+1, vector<int> (n+1, 0));
+        vector<vector<int>> dpArr(n+1, vector<int> (amount+1));
         
-        for(int i = 0; i <= amount; i++) dpArr[i][0] = INT_MAX-1;
-        for(int i = 0; i <= n; i++) dpArr[0][i] = 0;
-        for(int i = 1; i <= amount; i++) {
-            if(i % coins[0] == 0) dpArr[i][1] = i / coins[0];
-            else dpArr[i][1] = INT_MAX-1;
+        for(int i = 0; i <= amount; i++) dpArr[0][i] = INT_MAX - 1;
+        for(int i = 0; i <= n; i++) dpArr[i][0] = 0;
+        for(int i = 0; i <= amount; i++) {
+            if(i % coins[0] == 0) dpArr[1][i] = i / coins[0];
+            else dpArr[1][i] = INT_MAX - 1;
         }
         
-        for(int i = 1; i <= amount; i++) {
-            for(int j = 2; j <= n; j++) {
-                if(coins[j-1] > i) dpArr[i][j] = dpArr[i][j-1];
-                else {
-                    int diff = i - coins[j-1];
-                    dpArr[i][j] = min(dpArr[i][j-1], 1 + dpArr[diff][j]);
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(j - coins[i-1] >= 0) {
+                    dpArr[i][j] = min(dpArr[i-1][j], 1 + dpArr[i][j - coins[i-1]]);
+                } else {
+                    dpArr[i][j] = dpArr[i-1][j];
                 }
             }
         }
         
-        if(dpArr[amount][n] > amount) return -1;
-        return dpArr[amount][n];
+        if(dpArr[n][amount] > amount) return -1;
+        return dpArr[n][amount];
     }
 };
