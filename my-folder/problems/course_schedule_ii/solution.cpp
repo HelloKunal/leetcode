14ef288d8ab863fed5ca2b437cc26f6c95764334
Kunal <hellokunalpawar@gmail.com>
2022-05-27 12:04:12 +0000

    Sync LeetCode submission - Course Schedule II (cpp)

--- my-folder/problems/course_schedule_ii/solution.cpp
+++ my-folder/problems/course_schedule_ii/solution.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> inorderCol(numCourses, 0);
+        vector<int> inorderSize(numCourses, 0);
         vector<int> adj[numCourses];
-        for(auto p : prerequisites) {
-            inorderCol[p[0]]++;
-            adj[p[1]].push_back(p[0]);
+        for(auto i : prerequisites) {
+            inorderSize[i[0]]++;
+            adj[i[1]].push_back(i[0]);
         }
         
         queue<int> freeNodes;
         for(int i = 0; i < numCourses; i++) {
-            if(inorderCol[i] == 0) freeNodes.push(i);
-        }
-        
-        vector<int> res;
-        while(freeNodes.size() > 0) {
-            int freeNode = freeNodes.front();
-            freeNodes.pop();
-            res.push_back(freeNode);
-            
-            for(int v : adj[freeNode]) {
-                if(--inorderCol[v] == 0) {
-                    freeNodes.push(v);
-                }
+            if(inorderSize[i] == 0) {
+                freeNodes.push(i);
             }
         }
         
+        vector<int> res;
+        while(freeNodes.size()) {
+            int curr = freeNodes.front();
+            freeNodes.pop();
+            
+            res.push_back(curr);
+            
+            for(int v : adj[curr]) {
+                if(--inorderSize[v] == 0) {
+                    freeNodes.push(v);
+                }
+            }            
+        }
+        
         if(res.size() != numCourses) return {};
-        else return res;
+        return res;
     }
 };
