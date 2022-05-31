13f98c035e242b1f2703c398aad834c068d41451
Kunal <hellokunalpawar@gmail.com>
2022-05-31 14:53:55 +0000

    Sync LeetCode submission - Max Area of Island (cpp)

--- my-folder/problems/max_area_of_island/solution.cpp
+++ my-folder/problems/max_area_of_island/solution.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int maxArea;
-    int d[5] = {0, 1, 0, -1, 0};
-    void dfs(int &area, int x, int y, int xAxis, int yAxis, vector<vector<int>> &grid) {
-        area++;
-        maxArea = max(area, maxArea);
+    int res = 0, count = 0;
+    int dxy[5] = {0, 1, 0, -1, 0};
+    
+    void dfs(vector<vector<int>> &grid, int x, int y) {
         grid[y][x] = 2;
+        count++;
+        
         for(int i = 0; i < 4; i++) {
-            int nx = x + d[i];
-            int ny = y + d[i+1];
-            if(nx < 0 || nx > xAxis-1 || ny < 0 || ny > yAxis-1) continue;
-            if(grid[ny][nx] == 2 || grid[ny][nx] == 0) continue;
-            dfs(area, nx, ny, xAxis, yAxis, grid);            
+            int dx = x + dxy[i];
+            int dy = y + dxy[i+1];
+            
+            if(dx < 0 || dy < 0) continue;
+            if(dy >= grid.size() || dx >= grid[0].size()) continue;
+            if(grid[dy][dx] == 1) {
+                dfs(grid, dx, dy);
+            }
         }
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int xAxis = grid[0].size();
-        int yAxis = grid.size();
-        maxArea = 0;
-        for(int y = 0; y < yAxis; y++) {
-            for(int x = 0; x < xAxis; x++) {
-                if(grid[y][x] == 1) {                    
-                    int area = 0;
-                    dfs(area, x, y, xAxis, yAxis, grid);
+        for(int y = 0; y < grid.size(); y++) {
+            for(int x = 0; x < grid[0].size(); x++) {
+                if(grid[y][x] == 1) {
+                    count = 0;
+                    dfs(grid, x, y);
+                    res = max(res, count);
                 }
             }
         }
         
-        return maxArea;
+        return res;
     }
 };
