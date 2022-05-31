652a7552fd0e116520ed1926c13791179d974a57
Kunal <hellokunalpawar@gmail.com>
2022-05-31 16:15:18 +0000

    Sync LeetCode submission - Flood Fill (cpp)

--- my-folder/problems/flood_fill/solution.cpp
+++ my-folder/problems/flood_fill/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int d[5] = {0, 1, 0, -1, 0};
-    void dfs(int y, int x, int yAxis, int xAxis, int originalColor, int newColor, vector<vector<int>> &image) {
-        image[y][x] = newColor;
-        for(int i = 0; i < 4; i++) {
-            int ny = y + d[i+1];
-            int nx = x + d[i];
-            if(ny < 0 || ny > yAxis-1 || nx < 0 || nx > xAxis-1) continue;
-            if(image[ny][nx] != originalColor) continue;
-            dfs(ny, nx, yAxis, xAxis, originalColor, newColor, image);
-       }
+    int dxy[5] = {0, 1, 0, -1, 0};
+    
+    void dfs(vector<vector<int>>& grid, int i, int j, int newColor, int oldColor) {
+        grid[i][j] = newColor;
+        
+        for(int k = 0; k < 4; k++) {
+            int di = i + dxy[k];
+            int dj = j + dxy[k+1];
+            
+            if(di < 0 || dj < 0) continue;
+            if(di >= grid.size() || dj >= grid[0].size()) continue;
+            if(grid[di][dj] == oldColor) dfs(grid, di, dj, newColor, oldColor);
+        }        
     }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int xAxis = image[0].size();
-        int yAxis = image.size();
-        int originalColor = image[sr][sc];
-        if(newColor != originalColor) dfs(sr, sc, yAxis, xAxis, originalColor, newColor, image);
+        if(newColor != image[sr][sc]) dfs(image, sr, sc, newColor, image[sr][sc]);
         return image;
     }
 };
